c8c998c8debbd188b7b1654614916387496b74cd
Sarath chandra Viswanadh <sarathchandraviswanadh@Saraths-Air.attlocal.net>
2024-09-14 00:27:53 -0500

    Modified CSS files and made few changes to JSX files

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -3,11 +3,14 @@ import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import "../App.css";
+import Header from "../components/Header";
+import SignUp from "./SignUp";
 
 const Login = ({ onLogin }) => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [showSignUp, setShowSignUp] = useState(false);
 
   const submitHandler = async () => {
     try {
@@ -34,35 +37,55 @@ const Login = ({ onLogin }) => {
     }
   };
 
+  if (showSignUp) {
+    return <SignUp />;
+  }
+
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <Form>
-          <Form.Group className="mb-3">
-            <Form.Label>Email ID:</Form.Label>
-            <Form.Control
-              type="text"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </Form.Group>
+    <>
+      <Header />
+      <div className="login-container">
+        <div className="login-box">
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Email ID:</Form.Label>
+              <Form.Control
+                type="text"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
 
-          <Button variant="primary" type="button" onClick={submitHandler}>
-            <b>Submit</b>
-          </Button>
-          {message && <div className="alert alert-info mt-3">{message}</div>}
-        </Form>
+            <Button variant="primary" type="button" onClick={submitHandler}>
+              <b>Submit</b>
+            </Button>
+            {message && <div className="alert alert-info mt-3">{message}</div>}
+            <p>
+              Don't have an account?{" "}
+              <span
+                style={{
+                  cursor: "pointer",
+                  color: "blue",
+                  textDecoration: "underline",
+                }}
+                onClick={() => setShowSignUp(true)} // Change state to show SignUp
+              >
+                SignUp here!!
+              </span>
+            </p>
+          </Form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
